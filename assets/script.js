fce65f8530e4185131ec48c3788d92be633711db
alexandre110282 <69106721+alexandre110282@users.noreply.github.com>
2023-11-14 15:08:40 +0100

    FunctionBuilded

--- assets/script.js
+++ assets/script.js
@@ -16,36 +16,110 @@ const slides = [{
 	}
 ]
 
-
-let arrow = document.querySelectorAll(".arrow")
-console.log(arrow)
-console.log(slides.length)
+let arrowLeft = document.querySelector(".arrow_left")
+let arrowRight = document.querySelector(".arrow_right")
 let banner = document.getElementById("banner")
+let bannerImg = document.querySelector(".banner-img")
+let bannerP = document.querySelector("#banner p")
+let img = 0
+let tag = 0
+let nb = slides.length - 1
+console.log(bannerP)
+
+
+
+function changerTagLine() {
+	let cheminTag = `${slides[tag].tagLine}`
+	for (let i = 0; i < nb; i++) {
+		i === tag
+		bannerP.innerHTML = `${slides[tag].tagLine}`
+	}
+}
+arrowRight.addEventListener("click", function () {
+	tag++
+	if (tag > nb) {
+		tag = 0
+	}
+	changerTagLine()
+})
+
+arrowLeft.addEventListener("click", function () {
+	tag--
+	if (tag < 0) {
+		let nbElement = Object.keys(slides).length
+		tag = nbElement - 1
+	}
+	changerTagLine()
+})
 
 function changerImage() {
-	document.querySelector(".banner-img").src = "./assets/images/slideshow/slide2.jpg"
+	let cheminImg = `assets/images/slideshow/${slides[img].image}`
+	for (let i = 0; i < nb; i++) {
+		i === img
+		bannerImg.src = cheminImg
+	}
 }
 
-for (let i = 0; i < arrow.length; i++) {
-	arrow[i].addEventListener("click", function (event) {
-		if (arrow[i] === arrow[0]) {
-			changerImage()
-			console.log("Coucou!")
-		} else {
-			console.log("Bien joué")
-		}
-	})
-}
+arrowRight.addEventListener("click", function () {
+	img++
+	if (img > nb) {
+		img = 0
+	}
+	changerImage()
+})
 
+arrowLeft.addEventListener("click", function () {
+	img--
+	if (img < 0) {
+		let nbElement = Object.keys(slides).length
+		img = nbElement - 1
+	}
+	changerImage()
+})
 
+// Cibler la div "dots" et ajouter les bullets
 let dots = document.querySelector(".dots")
-console.log(dots)
-
 let bulletpoints = `
 	<span class = "dot"></span>
 `
-
-for (let index = 0; index < slides.length; index++) {
+for (let i = 0; i < slides.length; i++) {
 	dots.innerHTML += bulletpoints
-	console.log(dots)
 }
+//fin de l'ajout des bullets
+
+//cibler nouvel élément "dot" et définir sa variable
+let dotList = document.querySelectorAll(".dot")
+let nombreDot = 0
+
+
+// Création fonction pour ajouter ou retirer le fond des bullets en fonction de la taille 
+//du tableau, du nombre de bullet et sa position dans dotList
+function selected() {
+	for (let i = 0; i < slides.length; i++) {
+		if (i === nombreDot) {
+			dotList[i].classList.add("dot_selected");
+		} else {
+			dotList[i].classList.remove("dot_selected");
+		}
+	}
+}
+selected()
+
+arrowRight.addEventListener("click", function () {
+	nombreDot++
+	console.log("Coucou!")
+	if (nombreDot > slides.length - 1) {
+		nombreDot = 0
+	}
+	selected()
+})
+
+arrowLeft.addEventListener("click", function () {
+	nombreDot--
+	console.log("Bien joué")
+	if (nombreDot < 0) {
+		let nbElement = Object.keys(slides).length
+		nombreDot = nbElement - 1
+	}
+	selected()
+})
